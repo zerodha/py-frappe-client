bc704f8339d962d00971d53189c04ee384c880b9
mr-karan <karansharma1295@gmail.com>
2018-04-09 16:36:56 +0530

    Add README
    Summary: -
    
    Test Plan: -
    
    Reviewers: ramesh.ravi
    
    Reviewed By: ramesh.ravi
    
    Differential Revision: https://dev.zerodha.tech/D3856

---
+++ README.md
@@ -0,0 +1,60 @@
+# Py-Frappe-Client
+
+
+## Usage
+
+### Initialize
+```
+from frappe_client import FrappeRequest
+client = FrappeRequest(url="http://localhost:8002", username="user", password="password", callback=store_session_data)
+
+# Example Callback function
+ def store_session_data(data):
+    print ("Data stored is {}".format(data))
+
+
+>>> client = FrappeRequest(url="http://localhost:8002", username="Administrator", password="pwd", callback=store_session_data)
+Data stored is {'full_name': 'Administrator', 'sid': '427f585e17c551b9bf70ff64a22a6998064d0415914aaf91beb981f2', 'system_user': 'yes', 'user_id': 'Administrator', 'user_image': ''}
+
+```
+
+### Using Callback
+The wrapper checks for a HTTP 403 Response from the server for the first time on every API call. If the server
+returns 403, the wrapper attempts to login again. During this login, if a `callback` function is passed by the user
+the new `session_data` is passed on to this callback parameter.  
+
+### Get Session Data
+```
+client.session_data
+
+# Example Output
+# >>> client.session_data
+
+>>> {'full_name': 'Administrator',
+# 'sid': '427f585e17c551b9bf70ff64a22a6998064d0415914aaf91beb981f2',
+# 'system_user': 'yes',
+# 'user_id': 'Administrator',
+# 'user_image': ''}
+```
+
+### Get Endpoint call
+```
+client.get(method, params)
+
+# Example Request
+# In [13]: response = client.get(method="acop_process.lead_integration.ping")
+# In [15]: response.json()
+# Out[15]: {'message': 'PONG'}
+```
+
+
+### POST Endpoint call
+```
+client.get(method, params)
+
+# Example Request
+# In [20]: response = client.post(method="acop_process.lead_integration.post_ping", params={'test_param':123})
+# In [21]: response.json()
+Out[21]: {'message': 'POST PONG success 123'}
+```
+
--- frappe_client.py
+++ frappe_client.py
@@ -27,6 +27,8 @@ class FrappeRequest(object):
         self.session_data = None
         self.callback = callback
 
+        # If user provides `session_data` don't login again, 
+        # instead set the cookie data in requests.Session() object
         if session_data:
             self.session_data = session_data
             self.set_session_token(session_data) 
@@ -52,6 +54,8 @@ class FrappeRequest(object):
             raise GeneralException("Invalid Session")
         if login_response.status_code !=200:
             raise GeneralException("An error with frappe response occurred")
+        # If user provides a callback function, call the function with the 
+        # session data
         if self.callback:
             session_data = self._get_cookie_data(login_response)
             self.callback(session_data)
@@ -69,6 +73,7 @@ class FrappeRequest(object):
         """
         if session_data:
             cookiejar = requests.utils.cookiejar_from_dict(session_data)
+        # Set the cookies for future requests made by `self.frappe_session` object
         self.frappe_session.cookies = cookiejar
 
 
@@ -80,13 +85,36 @@ class FrappeRequest(object):
         Args:
             method (str): Endpoint to call
             params (dict): Dict representation of additional data to call
-        
+
         Returns:
             response (<requests.Response>): Response object received from the Frappe server
 
         """
         response = self.frappe_session.get(self.url +"/api/method/" + method + "/", params=params)
         if response.status_code == 403:
+            # For the 1st 403 response try logging again
+            login_response = self._login()
+            if login_response.status_code == 200:
+                response = self.frappe_session.get(self.url +"/api/method/" + method + "/", params=params)
+
+        return response
+
+    def post(self, method, params=None):
+        """
+        Wrapper around POST API requests. Handles the 1st 403 response
+        internally
+
+        Args:
+            method (str): Endpoint to call
+            params (dict): Dict representation of additional data to call
+
+        Returns:
+            response (<requests.Response>): Response object received from the Frappe server
+
+        """
+        response = self.frappe_session.post(self.url +"/api/method/" + method + "/", params=params)
+        if response.status_code == 403:
+            # For the 1st 403 response try logging again
             login_response = self._login()
             if login_response.status_code == 200:
                 response = self.frappe_session.get(self.url +"/api/method/" + method + "/", params=params)
